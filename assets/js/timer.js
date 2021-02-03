604d89dba89cfd0ff0e669c10bc7e5310602442c
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-03 13:34:05 -0700

    Add logic for onTick() and onComplete() methods

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,14 +1,25 @@
 class Timer {
-    constructor(startBtn, stopBtn, duration) {
+    constructor(startBtn, stopBtn, duration, callbacks) {
         this.startBtn = startBtn;
         this.stopBtn = stopBtn;
         this.duration = duration;
+
+        //if callbacks are passed in do the following
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         //watch for click events on the stop and start button.
         this.startBtn.addEventListener('click', this.start);
         this.stopBtn.addEventListener('click', this.pause);
     }
     start = () => {
+        //if onStart is valid, call this.onStart() callback function
+        if (this.onStart) {
+            this.onStart();
+        }
         this.tick(); //immediately run 1 tick upon starting.
         this.interval = setInterval(this.tick, 1000); //declaring as 'this.interval' allows us to share the data among methods
     }
@@ -16,7 +27,18 @@ class Timer {
         clearInterval(this.interval)
     }
     tick = () => {
-        this.timeRemaining = this.timeRemaining - 1
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 1
+            if (this.onTick) {
+                this.onTick();
+            }
+        }
+
         //below has been encapsulated into getters and setters
         // const timeRemaining = parseFloat(this.duration.value); //set the the default value set in index.html
         // this.duration.value = timeRemaining - 1
