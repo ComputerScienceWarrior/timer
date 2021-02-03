17bb1d3286e7344b21220e4dfc39e01e8a3511e9
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-03 12:53:39 -0700

    pass optional object of callback functions to timer {onStart, onTick, onComplete}

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,4 +4,14 @@ const start = document.querySelector("#start");
 const stop = document.querySelector("#stop");
 const duration = document.querySelector("#duration");
 
-const timer = new Timer(start, stop, duration);
+const timer = new Timer(start, stop, duration, {
+    onStart() {
+        console.log("Started Timer.")
+    },
+    onTick() {
+        console.log("Ticking")
+    },
+    onComplete() {
+        console.log("Timer finished.")
+    }
+})
