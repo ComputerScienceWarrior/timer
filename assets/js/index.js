a3706e4bdb492e00431053fc579315b5f9a9e215
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-02-03 16:13:51 -0700

    Create reference to circle SVG and adjust the stroke-dashoffset and stroke-dasharray properties dynamically.

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,23 @@ import Timer from './timer.js'
 const start = document.querySelector("#start");
 const stop = document.querySelector("#stop");
 const duration = document.querySelector("#duration");
+const circle = document.querySelector('circle')
+
+let currentOffset = 0;
+
+
+//here is where we calculate our 'stoke-dasharray' property..which is our perimeter
+//perimeter = 2 * PI * r
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
 const timer = new Timer(start, stop, duration, {
     onStart() {
         console.log("Started Timer.")
     },
     onTick() {
-        console.log("Ticking")
+        circle.setAttribute('stroke-dashoffset', currentOffset)
+        currentOffset -= 50
     },
     onComplete() {
         console.log("Timer finished.")
